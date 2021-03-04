3e75ba35acf50e4bfae75a067d4aaefb0bf8dfc9
aymenby <aymenby30@gmail.com>
2021-03-04 03:18:26 +0100

    add css

--- src/Component/Task.js
+++ src/Component/Task.js
@@ -12,7 +12,7 @@ const Task = props => {
           {props.task.isEdited ? <input className="form-control input_up"  value={field} name="text" type="text" onChange={(e)=> setfield(e.target.value)} /> : <h4 className="card-body justify-content-between">{props.task.text}</h4>}
             <button className="col-4 mb-1 btn btn-danger" onClick={()=> {dispatch(delete_task(props.task.id))}} >Delete</button>
             <button className="col-4 mb-1 btn btn-info" onClick={()=> props.task.isEdited ? dispatch(update_task(props.task.text=field,props.task.isEdited=false)) : dispatch(edit_task(props.task.id)) } >{props.task.isEdited ? 'Save Edit' : 'Edit'}</button>
-            <button className={props.task.complete ? 'col-4 mb-1 btn btn-warning' : 'col-4 mb-1 btn btn-success'} onClick={()=> {dispatch(complete_task(props.task.id))}}> {props.task.complete ? 'Undo' : 'Complete'} </button>
+            <button className={props.task.complete ? 'col-4 mb-1 btn btn-warning'  : 'col-4 mb-1 btn btn-success'} onClick={()=> {dispatch(complete_task(props.task.id))}}> {props.task.complete ? 'Undo' : 'Complete'} </button>
         </div>
     )
 }
